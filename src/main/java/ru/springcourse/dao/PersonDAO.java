0d7c3bb2c22f11c599484b205d646e2a10a94cd2
Denis Rogov <kpotuw@gmail.com>
2023-04-28 04:50:52 +0300

    lesson 21 ending

---
+++ src/main/java/ru/springcourse/dao/PersonDAO.java
@@ -0,0 +1,29 @@
+package ru.springcourse.dao;
+
+import org.springframework.stereotype.Component;
+import ru.springcourse.models.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private List<Person> people;
+    private static int peopleCount;
+
+    {
+        people = new ArrayList<>();
+        people.add(new Person(++peopleCount, "Tom"));
+        people.add(new Person(++peopleCount, "Bob"));
+        people.add(new Person(++peopleCount, "Mike"));
+        people.add(new Person(++peopleCount, "Katy"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+}
